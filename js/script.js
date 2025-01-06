43a07554a693e2a19e99fe5e8582a46b5426fcbb
XaydarovSardor <birbalobirbattar@gmail.com>
2025-01-06 16:29:03 +0500

    json added

--- js/script.js
+++ js/script.js
@@ -1,19 +1,26 @@
 window.addEventListener("DOMContentLoaded", () => {
+
     const loginBtn = document.querySelector(".login-btn")
     const loginModal = document.querySelector("#modalOverlay")
     const closeBtn = loginModal.querySelector(".close-button")
-    loginBtn.addEventListener("click", showLoginModal)
-    closeBtn.addEventListener("click", closeLoginModal)
+    loginBtn.addEventListener("click", () => {
+        showLoginModal()
+        document.body.style.overflow = "hidden"
+    })
+    closeBtn.addEventListener("click", () => {
+        closeLoginModal()
+        document.body.style.overflow = "visible"
+    })
     loginModal.addEventListener("click", (e) => {
-        if (e.target === loginModal) {
+        if (e.target === loginModal){
             closeLoginModal()
-
         }
     })
     function showLoginModal() {
         loginModal.classList.add("active")
     }
     function closeLoginModal() {
+        document.body.style.overflow = "visible"
         loginModal.classList.remove("active")
     }
 
@@ -23,13 +30,12 @@ window.addEventListener("DOMContentLoaded", () => {
     const loginForm = loginModal.querySelector("#loginForm")
     loginForm.addEventListener("submit", (e) => {
         e.preventDefault()
-        const nameInput = loginForm.querySelector(".name")
-        const emailInput = loginForm.querySelector(".email")
         const formData = new FormData(loginForm)
         const obj = {}
         formData.forEach((value, key) => {
             obj[key] = value
         })
+        
         closeLoginModal()
         deleteLoginBtn()
 
@@ -44,9 +50,118 @@ window.addEventListener("DOMContentLoaded", () => {
             deleteLoginBtn()
         }
     })
-    const seeList = document.querySelector(".full-list")
-    const fullList = document.querySelector(".more-posts")
-    seeList.addEventListener("click", ()=>{
-        fullList.classList.toggle("active")
-    })
-})
+
+    // blogs
+    const blogContainer = document.querySelector(".left-blogs")
+    axios.get("http://localhost:3000/blogs")
+        .then(response => {
+            let data = response.data
+            data.forEach(blog => {
+                let { authorImg, authorName, blogTitle, blogDesc, blogDate, likes, comments, postImg } = blog
+                blogContainer.innerHTML += `
+                <div class="blog">
+                    <div class="author">
+                        <img src="${authorImg}" alt="author">
+                        <h3 class="blog-author">${authorName}</h3>
+                    </div>
+                    <div class="card-info">
+                        <div class="card-content">
+                            <h2 class="card-content__title">${blogTitle}</h2>
+                            <h4 class="card-content__subtitle">${blogDesc}</h4>
+                            <div class="card-statistics">
+                                <div class="date">
+                                    <img src="./images/icons/date-icon.png" alt="date">
+                                    <span>${blogDate}</span>
+                                </div>
+                                <div class="likes">
+                                    <img src="./images/icons/like-icon.png" alt="like">
+                                    <span>${likes}</span>
+                                </div>
+                                <div class="commments">
+                                    <img src="./images/icons/comment.svg" alt="comment">
+                                    <span>${comments}</span>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="card-content__img">
+                            <img src="${postImg}"
+                                alt="card-content__img">
+                        </div>
+                    </div>
+                </div>
+                `
+            })
+
+        })
+
+    const postContainer = document.querySelector(".posts")
+    axios.get("http://localhost:3000/posts")
+        .then(response => {
+            let data = response.data
+            data.forEach(blog => {
+                let { authorImg, authorName, postTitle, postDate } = blog
+                postContainer.innerHTML += `
+                <div class="post">
+                    <div class="post-author">
+                        <img src="${authorImg}"
+                            alt="post-author">
+                        <span class="post-author__name"${authorName}</span>
+                    </div>
+                    <h3 class="post-title">${postTitle}</h3>
+                    <h4 class="date"><span>${postDate}</span></h4>
+                </div>
+
+                `
+            })
+        })
+    const morePostsContainer = document.querySelector(".more-posts")
+    axios.get("http://localhost:3000/morePosts")
+        .then(response => {
+
+            let data = response.data
+
+            data.forEach(post => {
+                let { authorImg, authorName, postDate, postTitle } = post
+                morePostsContainer.innerHTML += `
+                    <div class="post">
+                        <div class="post-author">
+                            <img src="${authorImg}"
+                                alt="post-author">
+                            <span class="post-author__name"${authorName}</span>
+                        </div>
+                        <h3 class="post-title">${postTitle}</h3>
+                        <h4 class="date"><span>${postDate}</span></h4>
+                    </div>
+                    `
+            })
+            const seeListBtn = document.querySelector(".full-list")
+            seeListBtn.addEventListener("click", () => {
+                document.body.classList.toggle("active")
+                console.log(morePostsContainer);
+            })
+
+        })
+
+    const followContainer = document.querySelector(".recomend-follows")
+    axios.get("http://localhost:3000/following")
+        .then(response => {
+            let data = response.data
+            data.forEach(following => {
+                let { authorImg, authorName, contentTitle, contentDesc } = following
+                followContainer.innerHTML += `
+                    <div class="recomend-follow">
+                        <img width="30" height="30"
+                            src="${authorImg}"
+                            alt="follow-img">
+                        <div class="recomend-follow__info">
+                            <h2 class="follow-name">${authorName}</h2>
+                            <p class="follow-job">${contentTitle}}</p>
+                            <p class="follow-desc">${contentDesc}</p>
+                        </div>
+                        <button class="following">Follow</button>
+                    </div>
+                    `
+            })
+        })
+})  
